cf76ca517734a4b6975522829955bfef3f07f426
kris <krisbien@gmail.com>
2022-01-21 16:28:22 +0100

    links

---
+++ ColorTools.cs
@@ -0,0 +1,35 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiaSharpTestApp
+{
+    public static class ColorTools
+    {
+        /// <summary>
+        /// Shades color (make lighter or darker).
+        /// </summary>
+        /// <param name="color"></param>
+        /// <param name="percent">any value from -1.0 to 1.0</param>
+        /// <returns>Shaded color</returns>
+        static public SKColor ShadeRGBColor(SKColor color, float percent)
+        {
+            // https://github.com/PimpTrizkit/PJs/wiki/12.-Shade,-Blend-and-Convert-a-Web-Color-(pSBC.js)#stackoverflow-archive-begin
+
+            byte t = (byte)(percent < 0 ? 0 : 255);
+            var p = percent < 0 ? percent * -1 : percent;
+            var R = color.Red;
+            var G = color.Green;
+            var B = color.Blue;
+
+            var rr = (byte)(Math.Round((t - R) * p) + R);
+            var gg = (byte)(Math.Round((t - G) * p) + G);
+            var bb = (byte)(Math.Round((t - B) * p) + B);
+
+            return new SKColor(rr, gg, bb);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -31,9 +32,111 @@ namespace SkiaSharpTestApp
             public float Radius;
             //public SKColor Color;
             public SKPaint Paint;
+
+            public List<PortDrawItem> Ports = new List<PortDrawItem>(1);
+
+            //public DrawItem(float x, float y, float radius)
+            //{
+            //    X = x;
+            //    Y = y;
+            //    Radius = radius;
+            //    Ports.Add(new PortDrawItem(x + Radius, y));
+            //}
+
+            public void Recalc()
+            {
+                foreach(var port in Ports)
+                {
+                    port.Point = new SKPoint(X + Radius, Y);
+                }
+            }
+
+            public void AddPort()
+            {
+                //Ports.Add(new PortDrawItem(x + Radius, y));
+                Ports.Add(new PortDrawItem());
+                Recalc();
+            }
         }
 
+        class PortDrawItem
+        {
+            const float Radius = 5f;
+            const float HoverRadius = Radius + 5f;
+
+            //const float RadiusSquared = Radius * Radius;
+
+            public SKPoint Point;
+            public bool IsHovered;
+
+            //public PortDrawItem(float x, float y)
+            //{
+            //    Point = new SKPoint(x, y);
+            //}
+
+            public void Draw(SKCanvas canvas)
+            {
+                canvas.DrawCircle(new SKPoint(Point.X, Point.Y), Radius, _paintDarkGreen);
+
+                if(IsHovered)
+                    canvas.DrawCircle(new SKPoint(Point.X, Point.Y), HoverRadius, _paintDarkGreenPen);
+            }
+
+            public bool HitTest(SKPoint hitPoint)
+            {
+                //return (Point-hitPoint).LengthSquared < RadiusSquared;
+                return (Point - hitPoint).Length < HoverRadius;
+            }
+
+            static SKPaint _paintDarkGreen = new SKPaint
+            {
+                IsAntialias = true,
+                Color = SKColors.DarkGreen,
+                StrokeCap = SKStrokeCap.Square
+            };
+
+            static SKPaint _paintDarkGreenPen = new SKPaint
+            {
+                IsAntialias = true,
+                Color = SKColors.DarkGreen,
+                StrokeCap = SKStrokeCap.Square,
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 2f
+            };
+        } // end class
+
+        class Connection
+        {
+            public DrawItem FromItem;
+            public int FromPortIdx;
+
+            public DrawItem ToItem;
+            public int ToPortIdx;
+
+            public bool IsHovered = false;
+
+            public Connection(DrawItem fromItem, int fromPortIdx, DrawItem toItem, int toPortIdx)
+            {
+                FromItem = fromItem;
+                FromPortIdx = fromPortIdx;
+                ToItem = toItem;
+                ToPortIdx = toPortIdx;
+            }
+
+            
+
+
+            //public ConnectionEnd(DrawItem fromItem, int toPortIdx)
+            //{
+            //    DrawItem = item;
+            //    PortIdx = portIdx;
+            //}
+        }
+
+        List<Connection> _connectionItems = new List<Connection>();
+
         List<DrawItem> _drawItems = new List<DrawItem>();
+        //List<PortDrawItem> _portItems = new();
 
         //private readonly DrawingBox _box;
         private SKMatrix _matrix;
@@ -55,11 +158,12 @@ namespace SkiaSharpTestApp
 
             ctrSkElement.MouseLeave += CtrSkElement_MouseLeave;
 
-            const int count = 10;
+            // *** create items for a test
+            const int count = 20;
             _drawItems = new List<DrawItem>(count);
 
             var perRow = (int)Math.Sqrt(count);
-            const int size = 40;
+            const int diam = 40;
             const int margin = 100;
             const int off = 100;
             for (int i = 0; i < count; i++)
@@ -68,13 +172,18 @@ namespace SkiaSharpTestApp
                 int col = i % perRow;
 
                 _drawItems.Add(new DrawItem { 
-                    X = off + col * (size + margin),
-                    Y = off + row * (size + margin),
-                    Radius = size/2,
+                    X = off + col * (diam + margin),
+                    Y = off + row * (diam + margin),
+                    Radius = diam/2,
                     Paint = _paint
                 });
             }
-            
+
+            //_portItems = new List<PortDrawItem>(_drawItems.Count);
+            foreach(DrawItem it in _drawItems)
+            {
+                it.AddPort();
+            }
 
             //ctrSkElement.DragEnter
             //Draw(ctrSkElement);
@@ -93,22 +202,69 @@ namespace SkiaSharpTestApp
         private bool _isMoveItemMode = false;
         private DrawItem? _moveItem;
 
+        // *** connection drawing when connecting
+        private bool _isConnectionDrawMode = false;
+        private SKPoint _connectionToPoint;
+        private SKPoint _connectionFromPoint;
+
+        private DrawItem? _connectionFromItem = null;
+        //private DrawItem? _connectionToItem = null;
+
+        private int _connectionFromPortIdx = -1;
+        //private int _connectionToPortIdx = -1;
+
+        // ***
+
+        enum HitType { None, Elem, Port, Conn };
+        struct HitResult
+        {
+            public HitType HitType;
+            public DrawItem? Item;
+            public int PortIdx;
+        }
+
         private void CtrSkElement_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            SKPath pp;
+
             var orgPoint = GetWorldPointFromMouseEvent(e);
             var pointWnd = GetPointFromMouseEvent(e);
 
-            Logger.Trace(">> MouseDown");
+            //Logger.Trace(">> MouseDown");
 
-            var hitItem = GetHitTestItem(orgPoint);
+            var hitResult = GetHitTestItem(orgPoint);
 
-            if (hitItem != null)
+            if (hitResult.HitType == HitType.Conn)
             {
+
+            }
+            else if (hitResult.HitType == HitType.Port)
+            {
+                // *** CONNECTING
+                //if (!_isConnectionDrawMode)
+                {
+                    Logger.Trace(">> CONNECT - FIRST END");
+
+                    _connectionFromPoint = orgPoint;
+                    _connectionToPoint = orgPoint;
+
+                    _connectionFromPortIdx = hitResult.PortIdx;
+                    _connectionFromItem = hitResult.Item;
+
+                    _isConnectionDrawMode = true;
+                }
+   
+            }
+            else if (hitResult.HitType == HitType.Elem)
+            {
+                // *** MOVING
+
                 ctrSkElement.CaptureMouse();
                 //ctrSkElement.Cursor = Cursors.SizeAll; //delay till first mouse move
 
                 _isMoveItemMode = true;
-                _moveItem = hitItem;
+                _moveItem = hitResult.Item;
+                Debug.Assert(_moveItem != null);
                 _startMove = new SKPoint(_moveItem.X, _moveItem.Y) - orgPoint; // here as offset in world coord
                 _movedMouse = false;
             }
@@ -124,11 +280,13 @@ namespace SkiaSharpTestApp
 
                 _isPanMode = true;
                 _startMove = pointWnd;
+
             }
         }
 
         private void CtrSkElement_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            // *** pan mode
             ctrSkElement.ReleaseMouseCapture();
             ctrSkElement.Cursor = Cursors.Arrow;
 
@@ -138,15 +296,55 @@ namespace SkiaSharpTestApp
             // *** move mode
             _isMoveItemMode = false;
             _moveItem = null;
+
+            // *** PORT CONNECTION
+            if (_isConnectionDrawMode)
+            {
+                Logger.Trace(">> CONNECT - SECOND END");
+
+                var orgPoint = GetWorldPointFromMouseEvent(e);
+                //var pointWnd = GetPointFromMouseEvent(e);
+
+                var hitResult = GetHitTestItem(orgPoint);
+                if (hitResult.HitType == HitType.Port)
+                {
+                    var connectionToPortIdx = hitResult.PortIdx;
+                    var connectionToItem = hitResult.Item;
+
+                    if ( (connectionToItem == _connectionFromItem && connectionToPortIdx == _connectionFromPortIdx) ) 
+                    {
+                        // A -> A is not allowed
+                        SystemSounds.Beep.Play();
+                    }
+                    else
+                    {
+                        Debug.Assert(_connectionFromItem != null);
+                        Debug.Assert(connectionToItem != null);
+
+                        var ci = new Connection(_connectionFromItem, _connectionFromPortIdx, connectionToItem, connectionToPortIdx);
+                        _connectionItems.Add(ci);
+                    }
+                }
+
+            }
+
+            _isConnectionDrawMode = false;
+
+            ctrSkElement.InvalidateVisual();
         }
 
-        private void HandlePanOnMouseMove(SKPoint point)
+        private void HandlePanOnMouseMove(SKPoint pointWnd, SKPoint orgPoint)
         {
             if (_isPanMode)
             {
-                var off = (point - _startMove);
+                var off = (pointWnd - _startMove);
                 _matrix = _matrix.PostConcat(SKMatrix.CreateTranslation(off.X, off.Y));
-                _startMove = point;
+                _startMove = pointWnd;
+            }
+
+            if(_isConnectionDrawMode)
+            {
+                _connectionToPoint = orgPoint;
             }
         }
 
@@ -167,6 +365,7 @@ namespace SkiaSharpTestApp
                 var newPos = orgPoint + _startMove;
                 _moveItem.X = newPos.X;
                 _moveItem.Y = newPos.Y;
+                _moveItem.Recalc();
             }
         }
 
@@ -242,7 +441,7 @@ namespace SkiaSharpTestApp
 
             DoHitTest(orgPoint);
 
-            HandlePanOnMouseMove(pointWnd);
+            HandlePanOnMouseMove(pointWnd, orgPoint);
             HandleMoveItemOnMouseMove(pointWnd, orgPoint);
 
             //if (DoHitTest(orgPoint))
@@ -269,36 +468,129 @@ namespace SkiaSharpTestApp
 
         private bool DoHitTest(SKPoint? point)
         {
-            return GetHitTestItem(point) != null;
+            return GetHitTestItem(point).HitType != HitType.None;
         }
 
 
-        private DrawItem? GetHitTestItem(SKPoint? point)
+        SKPath aLine = new SKPath();
+        SKPath aLineRender = new SKPath();
+
+        private HitResult GetHitTestItem(SKPoint? point1)
         {
+
             //DrawItem[] cc = new DrawItem[2];
 
-            int hitCount = 0;
-            DrawItem hitItem = null;
+            //int hitCount = 0;
 
-            foreach (var it in _drawItems)
-            //for (int i = 0; i < _drawItems.Count; i++)
+
+            if (point1 == null)
             {
-                //ref DrawItem it = ref _drawItems[i];
+                // *** OUTSIDE VIEW
 
-                if(point == null)
+                foreach (var it in _drawItems)
                 {
-                    it.Paint = _paint;
-                    continue;
+                    for (int i = 0; i < it.Ports.Count; i++)
+                    {
+                        PortDrawItem? pp = it.Ports[i];
+                        pp.IsHovered = false;
+                    }
                 }
 
+                foreach (var cc in _connectionItems)
+                {
+                    cc.IsHovered = false;
+                }
+
+                foreach (var it in _drawItems)
+                {
+                    it.Paint = _paint;
+                }
+
+                return new HitResult
+                {
+                    HitType = HitType.None,
+                    Item = null,
+                    PortIdx = -1
+                };
+            }
+
+            
+
+            // *** INSIDE VIEW
+
+            var point = point1.Value;
+
+            DrawItem? hitItem = null;
+
+            HitType hitType = HitType.None;
+            int portIdxHit = -1;
+
+            bool found = false;
+            // *** fist goes for ports
+            foreach (var it in _drawItems)
+            {
+                for (int i = 0; i < it.Ports.Count; i++)
+                {
+                    PortDrawItem? pp = it.Ports[i];
+
+                    pp.IsHovered = pp.HitTest(point);
+
+                    if (pp.IsHovered)
+                    {
+                        found = true;
+                        hitType = HitType.Port;
+                        portIdxHit = i;
+                        hitItem = it;
+                        break; // port hit but not element
+                    }
+                }
+            }
+
+            // *** now goes for connections
+
+
+
+            //SKPath aLine = new SKPath();
+            //SKPath aLineRender = new SKPath();
+
+            foreach (var cc in _connectionItems)
+            {
+                aLine.Rewind();
+                aLineRender.Rewind();
+
+                aLine.MoveTo(cc.FromItem.Ports[cc.FromPortIdx].Point);
+                aLine.LineTo(cc.ToItem.Ports[cc.ToPortIdx].Point);
+
+                _paintDarkGreenPen_Hit.GetFillPath(aLine, aLineRender, 0.5f);
+
+                if(!found && aLineRender.Contains(point.X, point.Y))
+                {
+                    //SystemSounds.Beep.Play();
+                    cc.IsHovered = true;
+                    found = true;
+                    hitType = HitType.Conn;
+                }
+                else
+                {
+                    cc.IsHovered = false;
+                }
+            }
+
+
+            // *** now goes for nodes
+            foreach (var it in _drawItems)
+            {
+
                 
-                var vv = point.Value - new SKPoint(it.X, it.Y);
-                if(vv.Length <= it.Radius)
+                var vv = point - new SKPoint(it.X, it.Y);
+                if(!found && vv.Length <= it.Radius)
                 {
                     it.Paint = _paintHighlight;
-                    hitCount++;
+                    //hitCount++;
                     hitItem = it;
-                    break; // todo: handle many items
+                    hitType = HitType.Elem;
+                    found = true;
+                    //break; // todo: handle many items
                 }
                 else
                 {
@@ -306,7 +598,11 @@ namespace SkiaSharpTestApp
                 }
             }
 
-            return hitItem;
+            return new HitResult { 
+                HitType = hitType,
+                Item = hitItem,
+                PortIdx = portIdxHit
+            };
         }
 
         static SKColor _color = new SKColor(0x2c, 0x3e, 0x50);
@@ -321,7 +617,7 @@ namespace SkiaSharpTestApp
         SKPaint _paintHighlight = new SKPaint
         {
             IsAntialias = true,
-            Color = SKColors.BlueViolet,
+            Color = ColorTools.ShadeRGBColor(_color, 0.1f), // SKColors.BlueViolet,
             StrokeCap = SKStrokeCap.Round
         };
 
@@ -330,6 +626,42 @@ namespace SkiaSharpTestApp
             IsAntialias = true,
             Color = SKColors.Red,
             StrokeCap = SKStrokeCap.Square
+        };
+
+        SKPaint _paintDarkGreen = new SKPaint
+        {
+            IsAntialias = true,
+            Color = SKColors.DarkGreen,
+            StrokeCap = SKStrokeCap.Square
+        };
+
+        private static readonly SKColor darkGreen = SKColors.DarkGreen;
+
+        SKPaint _paintDarkGreenPen = new SKPaint
+        {
+            IsAntialias = true,
+            Color = darkGreen,
+            StrokeCap = SKStrokeCap.Square,
+            Style = SKPaintStyle.Stroke,
+            StrokeWidth = 2f
+        };
+
+        SKPaint _paintDarkGreenPen_Hover = new SKPaint
+        {
+            IsAntialias = true,
+            Color = ColorTools.ShadeRGBColor(darkGreen, +0.1f),
+            StrokeCap = SKStrokeCap.Square,
+            Style = SKPaintStyle.Stroke,
+            StrokeWidth = 3f
+        };
+
+        SKPaint _paintDarkGreenPen_Hit = new SKPaint
+        {
+            IsAntialias = true,
+            Color = ColorTools.ShadeRGBColor(darkGreen, +0.1f),
+            StrokeCap = SKStrokeCap.Square,
+            Style = SKPaintStyle.Stroke,
+            StrokeWidth = 5f
         };
 
         private void CtrSkElement_PaintSurface(object? sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
@@ -348,6 +680,40 @@ namespace SkiaSharpTestApp
             foreach(var it in _drawItems)
             {
                 canvas.DrawCircle(new SKPoint(it.X, it.Y), it.Radius, it.Paint);
+            }
+
+            // *** draw connections
+            foreach(var cc in _connectionItems)
+            {
+                var from = cc.FromItem.Ports[cc.FromPortIdx].Point;
+                var to = cc.ToItem.Ports[cc.ToPortIdx].Point;
+
+                if(cc.IsHovered)
+                    canvas.DrawLine(from, to, _paintDarkGreenPen_Hover);
+                else
+                    canvas.DrawLine(from, to, _paintDarkGreenPen);
+            }
+
+            // *** draw ports
+
+            //foreach (var it in _drawItems)
+            //{
+            //    canvas.DrawCircle(new SKPoint(it.X + it.Radius, it.Y), it.Radius/4.0f, _paintDarkGreen);
+
+            //    canvas.DrawCircle(new SKPoint(it.X + it.Radius, it.Y), it.Radius / 4.0f + 5.0f, _paintDarkGreenPen);
+            //}
+
+            foreach(var it in _drawItems)
+            {
+                foreach(var pp in it.Ports)
+                {
+                    pp.Draw(canvas);
+                }
+            }
+
+            if(_isConnectionDrawMode)
+            {
+                canvas.DrawLine(_connectionFromPoint, _connectionToPoint, _paintDarkGreenPen);
             }
         }
 
